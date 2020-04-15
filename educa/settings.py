c95073c2b758fac7a26d69123efe9410694ac475
lwairore <kwairore@gmail.com>
2020-04-16 00:11:11 +0300

    - Minor fix.

--- educa/settings.py
+++ educa/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static')
+    # os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 SITE_ID = 1
 
