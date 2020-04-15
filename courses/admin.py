b7378103883e68c7492d0b59edec56860ad57af7
lwairore <kwairore@gmail.com>
2020-04-15 21:01:41 +0300

    - Register Subject model and SubjectAdmin class.

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
 # Register your models here.
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
